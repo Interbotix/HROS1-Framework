d0803ea585118955916a0ff1d38afcf094d240d1
DresnerRobotics <Andrew@trossenrobotics.com>
2015-10-15 17:26:32 -0700

    Manual merge from https://github.com/r3n33 branch
    	modified:   Framework/src/motion/modules/Action.cpp
    	modified:   Framework/src/motion/modules/Walking.cpp
    	new file:   Linux/project/ps3_demo/start_edison_bluez5
    	new file:   Linux/project/tutorial/head_tracking/Makefile
    	new file:   Linux/project/tutorial/head_tracking/config.ini
    	new file:   Linux/project/tutorial/head_tracking/head_tracking
    	new file:   Linux/project/tutorial/head_tracking/main.cpp
    	new file:   Linux/project/tutorial/head_tracking/main.o
    	new file:   Linux/project/tutorial/head_tracking/www/LICENSE.txt
    	new file:   Linux/project/tutorial/head_tracking/www/favicon.ico
    	new file:   Linux/project/tutorial/head_tracking/www/functions.js
    	new file:   Linux/project/tutorial/head_tracking/www/index.html
    	modified:   Linux/project/walk_tuner/Makefile
    	modified:   Linux/project/walk_tuner/main.cpp
    	new file:   Linux/project/walk_tuner/www/functions.js
    	new file:   Linux/project/walk_tuner/www/index.html

---
+++ Linux/project/tutorial/head_tracking/main.cpp
@@ -0,0 +1,128 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 2011. 1. 4.
+ *      Author: robotis
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <libgen.h>
+#include <signal.h>
+
+#include "Camera.h"
+#include "mjpg_streamer.h"
+#include "LinuxDARwIn.h"
+
+#ifdef MX28_1024
+#define MOTION_FILE_PATH    ((char *)"../../../Data/motion_1024.bin")
+#else
+#define MOTION_FILE_PATH    ((char *)"../../../Data/motion_4096.bin")
+#endif
+#define INI_FILE_PATH       ((char *)"../../../Data/config.ini")
+
+#define M_INI   ((char *)"../../../Data/slow-walk.ini")
+#define SCRIPT_FILE_PATH    "script.asc"
+
+#define U2D_DEV_NAME0       "/dev/ttyUSB0"
+#define U2D_DEV_NAME1       "/dev/ttyUSB1"
+
+int isRunning = 1;
+LinuxCM730 linux_cm730(U2D_DEV_NAME0);
+CM730 cm730(&linux_cm730);
+
+// Define the exit signal handler
+void signal_callback_handler(int signum)
+{
+    //LinuxCamera::~LinuxCamera();
+    printf("Exiting program; Caught signal %d\r\n",signum);
+    isRunning = 0;
+}
+
+void change_current_dir()
+{
+    char exepath[1024] = {0};
+    if(readlink("/proc/self/exe", exepath, sizeof(exepath)) != -1)
+        chdir(dirname(exepath));
+}
+
+int main(void)
+{
+    //Register signal and signal handler
+    signal(SIGINT, signal_callback_handler);
+
+    printf( "\n===== Head tracking Tutorial for DARwIn =====\n\n");
+
+    change_current_dir();
+
+    minIni* ini = new minIni(INI_FILE_PATH);
+    Image* rgb_ball = new Image(Camera::WIDTH, Camera::HEIGHT, Image::RGB_PIXEL_SIZE);
+
+    LinuxCamera::GetInstance()->Initialize(0);
+    LinuxCamera::GetInstance()->LoadINISettings(ini);
+
+    mjpg_streamer* streamer = new mjpg_streamer(Camera::WIDTH, Camera::HEIGHT);
+
+    ColorFinder* ball_finder = new ColorFinder();
+    ball_finder->LoadINISettings(ini);
+    httpd::ball_finder = ball_finder;
+
+    BallTracker tracker = BallTracker();
+
+//////////////////// Framework Initialize ////////////////////////////
+    if(MotionManager::GetInstance()->Initialize(&cm730) == false)
+    {
+        linux_cm730.SetPortName(U2D_DEV_NAME1);
+        if(MotionManager::GetInstance()->Initialize(&cm730) == false)
+        {
+            printf("Fail to initialize Motion Manager!\n");
+            return 0;
+        }
+    }
+
+    Walking::GetInstance()->LoadINISettings(ini);
+    usleep(100);
+    MotionManager::GetInstance()->LoadINISettings(ini);
+
+    MotionManager::GetInstance()->AddModule((MotionModule*)Action::GetInstance());
+    MotionManager::GetInstance()->AddModule((MotionModule*)Head::GetInstance());
+    MotionManager::GetInstance()->AddModule((MotionModule*)Walking::GetInstance());
+    //MotionManager::GetInstance()->StartThread();
+    //LinuxMotionTimer::Initialize(MotionManager::GetInstance());
+    LinuxMotionTimer linuxMotionTimer;
+        linuxMotionTimer.Initialize(MotionManager::GetInstance());
+        linuxMotionTimer.Start();
+
+	MotionStatus::m_CurrentJoints.SetEnableBodyWithoutHead(false);
+	MotionManager::GetInstance()->SetEnable(true);
+	/////////////////////////////////////////////////////////////////////
+
+	Head::GetInstance()->m_Joint.SetEnableHeadOnly(true, true);
+
+	//Head::GetInstance()->m_Joint.SetPGain(JointData::ID_HEAD_PAN, 8);
+	//Head::GetInstance()->m_Joint.SetPGain(JointData::ID_HEAD_TILT, 8);
+
+    while(isRunning)
+    {
+        //usleep(10000);
+        Point2D pos;
+        LinuxCamera::GetInstance()->CaptureFrame();	
+
+        tracker.Process(ball_finder->GetPosition(LinuxCamera::GetInstance()->fbuffer->m_HSVFrame));
+
+		rgb_ball = LinuxCamera::GetInstance()->fbuffer->m_RGBFrame;
+        for(int i = 0; i < rgb_ball->m_NumberOfPixels; i++)
+        {
+            if(ball_finder->m_result->m_ImageData[i] == 1)
+            {
+                rgb_ball->m_ImageData[i*rgb_ball->m_PixelSize + 0] = 255;
+                rgb_ball->m_ImageData[i*rgb_ball->m_PixelSize + 1] = 0;
+                rgb_ball->m_ImageData[i*rgb_ball->m_PixelSize + 2] = 0;
+            }
+        }
+        streamer->send_image(rgb_ball);
+    }
+
+    return 0;
+}
--- Linux/project/walk_tuner/main.cpp
+++ Linux/project/walk_tuner/main.cpp
@@ -8,7 +8,7 @@
 #include <signal.h>
 #include <libgen.h>
 #include "cmd_process.h"
-//#include "mjpg_streamer.h"
+#include "mjpg_streamer.h"
 
 #define INI_FILE_PATH       "../../../Data/config.ini"
 
@@ -48,6 +48,9 @@ int main(int argc, char *argv[])
 		else
 			ini = new minIni(INI_FILE_PATH);
 
+	mjpg_streamer* streamer = new mjpg_streamer(0, 0);
+    httpd::ini = ini;
+    
     //////////////////// Framework Initialize ////////////////////////////
     if(MotionManager::GetInstance()->Initialize(&cm730) == false)
     {
